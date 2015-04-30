64f692440a99a81706eb09213b67c32c58daa7db
Mathieu Méa <mathieu.mea@gmail.com>
2015-04-30 11:22:53 -0400

    Initial Release.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# ca-toronto-ttc-light-rail-parser
+ca-toronto-ttc-light-rail-parser
+================================
+
 Parser for Toronto TTC Light Rail (Streetcar) data
---
+++ src/org/mtransit/parser/ca_toronto_ttc_light_rail/TorontoTTCLightRailAgencyTools.java
@@ -0,0 +1,153 @@
+package org.mtransit.parser.ca_toronto_ttc_light_rail;
+
+import java.util.HashSet;
+import java.util.Locale;
+import java.util.regex.Pattern;
+
+import org.mtransit.parser.DefaultAgencyTools;
+import org.mtransit.parser.Utils;
+import org.mtransit.parser.gtfs.data.GCalendar;
+import org.mtransit.parser.gtfs.data.GCalendarDate;
+import org.mtransit.parser.gtfs.data.GRoute;
+import org.mtransit.parser.gtfs.data.GTrip;
+import org.mtransit.parser.mt.data.MAgency;
+import org.mtransit.parser.mt.data.MDirectionType;
+import org.mtransit.parser.mt.data.MRoute;
+import org.mtransit.parser.mt.data.MSpec;
+import org.mtransit.parser.mt.data.MTrip;
+
+// http://www1.toronto.ca/wps/portal/contentonly?vgnextoid=96f236899e02b210VgnVCM1000003dd60f89RCRD
+// http://opendata.toronto.ca/TTC/routes/OpenData_TTC_Schedules.zip
+public class TorontoTTCLightRailAgencyTools extends DefaultAgencyTools {
+
+	public static void main(String[] args) {
+		if (args == null || args.length == 0) {
+			args = new String[3];
+			args[0] = "input/gtfs.zip";
+			args[1] = "../../mtransitapps/ca-toronto-ttc-light-rail-android/res/raw/";
+			args[2] = ""; // files-prefix
+		}
+		new TorontoTTCLightRailAgencyTools().start(args);
+	}
+
+	private HashSet<String> serviceIds;
+
+	@Override
+	public void start(String[] args) {
+		System.out.printf("Generating TTC light rail data...\n");
+		long start = System.currentTimeMillis();
+		this.serviceIds = extractUsefulServiceIds(args, this);
+		super.start(args);
+		System.out.printf("Generating TTC light rail data... DONE in %s.\n", Utils.getPrettyDuration(System.currentTimeMillis() - start));
+	}
+
+	@Override
+	public boolean excludeCalendar(GCalendar gCalendar) {
+		if (this.serviceIds != null) {
+			return excludeUselessCalendar(gCalendar, this.serviceIds);
+		}
+		return super.excludeCalendar(gCalendar);
+	}
+
+	@Override
+	public boolean excludeCalendarDate(GCalendarDate gCalendarDates) {
+		if (this.serviceIds != null) {
+			return excludeUselessCalendarDate(gCalendarDates, this.serviceIds);
+		}
+		return super.excludeCalendarDate(gCalendarDates);
+	}
+
+	@Override
+	public boolean excludeTrip(GTrip gTrip) {
+		if (this.serviceIds != null) {
+			return excludeUselessTrip(gTrip, this.serviceIds);
+		}
+		return super.excludeTrip(gTrip);
+	}
+
+	@Override
+	public Integer getAgencyRouteType() {
+		return MAgency.ROUTE_TYPE_LIGHT_RAIL;
+	}
+
+	@Override
+	public long getRouteId(GRoute gRoute) {
+		return Long.parseLong(gRoute.route_short_name); // using route short name as route ID
+	}
+
+	@Override
+	public String getRouteLongName(GRoute gRoute) {
+		return MSpec.cleanLabel(gRoute.route_long_name.toLowerCase(Locale.ENGLISH));
+	}
+
+	private static final String AGENCY_COLOR = "B80000"; // RED (AGENCY WEB SITE CSS)
+
+	@Override
+	public String getAgencyColor() {
+		return AGENCY_COLOR;
+	}
+
+	private static final String COLOR_00529F = "00529F"; // BLUE (NIGHT BUSES)
+
+	@Override
+	public String getRouteColor(GRoute gRoute) {
+		int rsn = Integer.parseInt(gRoute.route_short_name);
+		if (rsn >= 300 && rsn <= 399) { // Night Network
+			return COLOR_00529F;
+		}
+		return null; // use agency color instead of provided colors (like web site)
+	}
+
+	private static final String WEST = "west";
+	private static final String EAST = "east";
+	private static final String SOUTH = "south";
+	private static final String NORTH = "north";
+
+	@Override
+	public void setTripHeadsign(MRoute route, MTrip mTrip, GTrip gTrip) {
+		String gTripHeadsignLC = gTrip.trip_headsign.toLowerCase(Locale.ENGLISH);
+		if (gTripHeadsignLC.startsWith(EAST)) {
+			mTrip.setHeadsignDirection(MDirectionType.EAST);
+			return;
+		} else if (gTripHeadsignLC.startsWith(WEST)) {
+			mTrip.setHeadsignDirection(MDirectionType.WEST);
+			return;
+		} else if (gTripHeadsignLC.startsWith(NORTH)) {
+			mTrip.setHeadsignDirection(MDirectionType.NORTH);
+			return;
+		} else if (gTripHeadsignLC.startsWith(SOUTH)) {
+			mTrip.setHeadsignDirection(MDirectionType.SOUTH);
+			return;
+		}
+		int directionId = gTrip.direction_id;
+		String stationName = cleanTripHeadsign(gTrip.trip_headsign);
+		if (route.id == 500l) {
+			if (directionId == 0) {
+				mTrip.setHeadsignDirection(MDirectionType.WEST);
+				return;
+			} else if (directionId == 1) {
+				mTrip.setHeadsignDirection(MDirectionType.EAST);
+				return;
+			}
+		}
+		mTrip.setHeadsignString(stationName, directionId);
+		System.out.println("Unexpected trip " + gTrip);
+		System.exit(-1);
+	}
+
+	@Override
+	public String cleanTripHeadsign(String tripHeadsign) {
+		return MSpec.cleanLabel(tripHeadsign);
+	}
+
+	private static final Pattern AT = Pattern.compile("( at )", Pattern.CASE_INSENSITIVE);
+	private static final String AT_REPLACEMENT = " / ";
+
+	@Override
+	public String cleanStopName(String gStopName) {
+		gStopName = gStopName.toLowerCase(Locale.ENGLISH);
+		gStopName = AT.matcher(gStopName).replaceAll(AT_REPLACEMENT);
+		gStopName = MSpec.cleanNumbers(gStopName);
+		return MSpec.cleanLabel(gStopName);
+	}
+}
